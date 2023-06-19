ae2b73342f3bd1149dd91d882cda4b29f56eeb28
Afrin Hasan Safin <114832343+Safin178@users.noreply.github.com>
2023-06-19 19:26:31 +0600

    Update temp.cpp

--- CP/temp.cpp
+++ CP/temp.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main() 
 {
   /*
+  for cp topic - https://www.youtube.com/playlist?list=PLauivoElc3ggagradg8MfOZreCMmXMmJ-
+
+for syntax- https://www.youtube.com/playlist?list=PLfqMhTWNBTe0b2nM6JHVCnAkhQRGiZMSJ
 
   */
    
