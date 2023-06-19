457aacc38c737701ea4ace3bf8e8bd96795e97c3
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-06-19 20:13:33 +0600

    notes

---
+++ CP/Alpha-21/s011.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main() 
+{
+  /*
+    language shika 10 %
+    problem solving 90%
+    beshi beshi problem solve na korle hobe na
+    Exam hobe -
+    basic if else
+    basic loop - pattern
+    array
+    string
+    ekto advanced - prefix sum
+    ques - https://telegra.ph/Question-cp1--Au21-06-19
+
+    
+  */
+   
+  
+   
+
+    
+
+    
+
+    
+
+  
+   return 0;
+}
