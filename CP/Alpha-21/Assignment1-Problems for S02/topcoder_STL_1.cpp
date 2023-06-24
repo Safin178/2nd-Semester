a0f28bef0814201291d6c1327b978f9eae86a811
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-06-24 11:35:53 +0600

    STL

---
+++ CP/Alpha-21/Assignment1-Problems for S02/topcoder_STL_1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+void solve()
+{
+    int x;
+    vector<int> v;
+    for(i = 0; i < 5; i++)
+    {
+        cin >> x;
+        v.push_back(x);
+    }
+
+
+
+
+
+
+
+
+
+}
+int main(){
+    int t=1;
+    //cin >> t;          // removr '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}
