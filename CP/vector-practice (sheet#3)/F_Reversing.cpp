82633820e56ae765004a8b1563b2c4adcc93ff54
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-06-26 17:48:30 +0600

    ac

---
+++ CP/vector-practice (sheet#3)/F_Reversing.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<long long> vl;
+typedef vector<int> vi;
+
+#define all(v)	((v).begin()), ((v).end())
+#define sz(v)	((int)((v).size()))
+#define ll long long
+#define pb push_back
+#define endll '\n'
+#define CY cout<<"YES\n";
+#define CN cout<<"NO\n";
+#define CYR cout<<"YES\n";return;
+#define CNR cout<<"NO\n";return;
+#define vit vector<int>::iterator  
+void printVec(vector<int> v2, string s = "")//vector ke print kore just printVec(v) likle hobe
+{
+	cout<<s;
+	for (int i = 0; i < (int)v2.size(); ++i)
+			cout<<v2[i]<<" ";
+		cout<<"\n";
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi v(n);
+    for(auto &x : v) cin >> x;
+    
+    vit it;
+    for(auto it= v.rbegin() ; it != v.rend() ; it++)//eikane reverse just 
+    {
+        cout << *it << " ";
+    }
+
+
+
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}
