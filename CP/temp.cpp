c35fe45076a1ca087ca1ae19beecae2dfa62e433
Afrin Hasan Safin <114832343+Safin178@users.noreply.github.com>
2023-06-27 15:53:45 +0600

    Update temp.cpp

--- CP/temp.cpp
+++ CP/temp.cpp
@@ -1,24 +1,48 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-int main() 
+
+typedef vector<long long> vl;
+typedef vector<int> vi;
+
+#define all(v)	((v).begin()), ((v).end())
+#define sz(v)	((int)((v).size()))
+#define ll long long
+#define pb push_back
+#define endll '\n'
+#define CY cout<<"YES\n"
+#define CN cout<<"NO\n"
+#define CYR cout<<"YES\n";return;
+#define CNR cout<<"NO\n";return;
+#define vit vector<int>::iterator  
+#define forcin(n) for(auto &x : n) cin>>x;
+#define forcout(n) for(auto x : n) cout << x << " ";
+void printVec(vector<int> v2, string s = "")//vector ke print kore just printVec(v) likle hobe
 {
-  /*
-  for cp topic - https://www.youtube.com/playlist?list=PLauivoElc3ggagradg8MfOZreCMmXMmJ-
+	cout<<s;
+	for (int i = 0; i < (int)v2.size(); ++i)
+			cout<<v2[i]<<" ";
+		cout<<"\n";
+}
 
-for syntax- https://www.youtube.com/playlist?list=PLfqMhTWNBTe0b2nM6JHVCnAkhQRGiZMSJ
-
-  */
-   
-  
-   
-
+void solve()
+{
+    int n;
+    cin >> n;
+    vi v(n);
+    for(auto &x : v) cin >> x;
+    
+    vit it;
     
 
-    
 
-    
 
-  
-   return 0;
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
 }
