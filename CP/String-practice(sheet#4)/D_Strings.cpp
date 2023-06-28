5cdf50790cd658da18ebce0165252ba00a8ac3c8
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-06-28 17:06:38 +0600

    Used built-in-function  a lot , AC

---
+++ CP/String-practice(sheet#4)/D_Strings.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<long long> vl;
+typedef vector<int> vi;
+
+#define all(v)	((v).begin()), ((v).end())
+#define sz(v)	((int)((v).size()))
+#define ll long long
+#define pb push_back
+#define endll '\n'
+#define CY cout<<"YES\n";
+#define CN cout<<"NO\n";
+#define CYR cout<<"YES\n";return;
+#define CNR cout<<"NO\n";return;
+#define vit vector<int>::iterator  
+#define forcin(n) for(auto &x : n) cin>>x;
+#define forcout(n) for(auto x : n) cout << x << " ";
+void printVec(vector<int> v2, string s = "")//vector ke print kore just printVec(v) likle hobe
+{
+	cout<<s;
+	for (int i = 0; i < (int)v2.size(); ++i)
+			cout<<v2[i]<<" ";
+		cout<<"\n";
+}
+
+void solve()
+{
+    string a , b ; 
+    cin >> a >> b;
+
+    cout << sz(a) << " " << sz(b) << endll;
+
+    cout << (a+b) << endll;
+
+    swap(a[0], b[0]);// didn't knew eta kora jai;
+
+    cout << a << " " << b << endll;
+
+
+
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}
