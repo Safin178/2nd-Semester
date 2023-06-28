f63009c8a988edc275c5e2ecba236685077d7059
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-06-28 15:50:19 +0600

    AC

--- CP/vector-practice (sheet#3)/Z_Binary_Search.cpp
+++ CP/vector-practice (sheet#3)/Z_Binary_Search.cpp
@@ -15,7 +15,7 @@ typedef vector<int> vi;
 #define CNR cout<<"NO\n";return;
 #define vit vector<int>::iterator  
 #define forcin(n) for(auto &x : n) cin>>x;
-#define forcout(n) for(auto x : n) cout << x << " ";AC
+#define forcout(n) for(auto x : n) cout << x << " ";
 void printVec(vector<int> v2, string s = "")//vector ke print kore just printVec(v) likle hobe
 {
 	cout<<s;
@@ -24,34 +24,30 @@ void printVec(vector<int> v2, string s = "")//vector ke print kore just printVec
 		cout<<"\n";
 }
 
-
 void solve()
 {
-    ll int n, q;
+    int n,q;
     cin >> n >> q;
-    vl v(n), p;
-    p.pb(0);
+    vi v(n);
+    for(auto &x : v) cin >> x;
     
-   ll int sum = 0;
-    
-    for(auto &x : v) 
-    {
-        cin >> x;
-        sum = sum + x;// *it diye korte caisi kinto hocce na don't know why
-        p.pb(sum);
-        
-         
-    }
 
+    sort(v.begin() , v.end());
+    vit it = v.begin();
     while(q--)
     {
-        ll int l , r;
-        cin >> l >> r;
-        cout << p[r] - p[l-1] << endll;
+        int num;
+        cin >> num;
+        bool y = binary_search(v.begin() , v.end() , num);
+        if(y)
+        {
+            cout << "found\n";
+        }
+        else
+        {
+            cout << "not found\n";
+        }
     }
-
-   
- 
     
 
 
