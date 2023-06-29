f8c6cf8528b87f0f419c51dc288748fb8790ddde
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-06-29 14:08:09 +0600

    ac

--- CP/String-practice(sheet#4)/H_Good_or_Bad.cpp
+++ CP/String-practice(sheet#4)/H_Good_or_Bad.cpp
@@ -52,3 +52,52 @@ int main(){
     }
     return 0;
 }
+
+// eta c diye korsilam
+/*
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    
+   int t;
+   scanf(" %d ", &t);
+   while(t--)
+   {
+        char s[100000];
+        scanf("%s", s);
+        int f = 0, i , j;
+        for(j= 0; j < (strlen(s)-2) ; j++)
+        {
+            //for(j = 0; j <= i; j++)
+            //{
+                if(((s[j] == '0') && (s[j+1] == '1') && (s[j+2] == '0')) || ((s[j] == '1') && (s[j+1] == '0') && (s[j+2] == '1')))
+                {
+                 f = 1;break;}
+            //}
+
+        }
+        if(f == 1)
+        {
+            printf("Good\n");
+        }
+        else
+        printf("Bad\n");
+
+   }
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+*/
