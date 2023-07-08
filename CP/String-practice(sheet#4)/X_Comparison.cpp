e87c94c4f96a8ea5c2180f61346bc47fce42160f
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-07-08 17:24:39 +0600

    ac

---
+++ CP/String-practice(sheet#4)/X_Comparison.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define sz(n) (int)(n).size()
+
+void solve2()
+{// question porte hobe balo moto
+    string s;
+    int i=0;
+    cin >> s;
+    string x,y,z,ans=s;
+    for(int i = 0; i < sz(s)-1; i++)
+    {
+        x = s.substr(0, i+1);
+        y=s.substr(i+1);
+        sort(x.begin(), x.end());
+        sort(y.begin(), y.end());
+        z = x + y;
+        if(z < ans)
+        {
+            ans = z;
+        }
+        //cout << z << endl;
+    }
+    cout << ans;
+    
+
+}
+
+// void solve(void)
+// {
+//     char s[100000];
+//     scanf("%s", s);
+//     int n = strlen(s);
+//     int i , j, temp;
+//     for( i = 1; i < n; i++)
+//     {
+//         for( j = 1; j < n; j++)
+//         {
+//             if(s[i] < s[j])
+//             {
+//                 temp = s[i];
+//                 s[i] = s[j];
+//                 s[j] = temp;
+//             }
+//         }
+//     }
+
+//     printf("%s\n", s);
+    
+// }
+
+int main()
+{
+    int t = 1;
+    //scanf("%d", &t);   // remove '//' for test case
+    
+    while(t--)
+    {
+        //solve(); wa astese
+        solve2();
+    }
+    return 0;
+}
