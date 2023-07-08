935a1b3008d7fd0e7839fbe1277a97d3c414468a
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-07-08 12:37:54 +0600

    ac

--- CP/String-practice(sheet#4)/L_String_Functions.cpp
+++ CP/String-practice(sheet#4)/L_String_Functions.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+#define forcin(n) for(auto &x : n) cin >> x
+#define forcout(n) for(auto x : n) cout << x << " "
+#define sz(n) (int)n.size()
+#define el endl
+
+void printVec(vector<int> v , string s=""){
+    cout << s;
+    for(int i=0;i<(int)v.size();i++)
+        cout << v[i] << " ";
+    cout << "\n";
+}
+
+
 #define ll long long
 void solve()
 {
