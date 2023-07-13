9b850dceb59c10b1490340e673c534b713f453e4
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-07-13 06:07:53 +0600

    try

---
+++ OOP/OOP1.cpp
@@ -0,0 +1,28 @@
+// OOP
+#include<bits/stdc++.h>
+using namespace std;
+class student{
+    public:
+
+    int age;
+    string name;
+
+
+};
+
+int main()
+{
+   student m;
+//    m.age = 20;
+//    m.name = "safin";
+
+//    cout << m.age << " " << m.name << "\n";
+
+//deep copy te C er value change korle a ki change hobe?
+
+
+
+
+
+   return 0;
+}
---
+++ OOP/prb1.cpp
@@ -0,0 +1,43 @@
+// OOP
+#include<bits/stdc++.h>
+using namespace std;
+
+//https://telegra.ph/Assignment-1-1221-07-12
+
+class Circle {
+    public:
+    int radius;
+
+    void Setradius(int r) {
+        radius = r;
+    }
+
+    double Cal_area(){
+        return 3.14159 * radius * radius;
+    }
+
+    void details()
+    {
+        cout << "Radius = " << radius << "\n";
+        cout << "Area = " << Cal_area() << "\n";
+    }
+    
+
+
+};
+
+int main()
+{
+   Circle a;
+
+   a.Setradius(5);
+
+   a.details();
+
+
+
+
+
+
+   return 0;
+}
