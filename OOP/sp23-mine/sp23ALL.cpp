cfd21a42fec84d5818d2594179875e46cd8e1a0d
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-08-24 14:28:10 +0600

    sp 23 2b korsi

---
+++ OOP/sp23-mine/sp23ALL.cpp
@@ -0,0 +1,30 @@
+
+
+// ! 2b
+/*
+#include<iostream>
+using namespace std;
+class flower{
+
+    int PL, LL;
+public:
+    flower(int PL, int LL)
+    {
+        this -> PL = PL;//eikane this diye korte hobe this -> PL, eikane PL upore private kora PL ke mean kortese
+        this -> LL = LL;
+    }
+    void show()
+    {
+
+        cout << PL << " " << LL << endl;
+    }
+
+
+};
+int main()
+{
+    flower f1(12,10), f2(1,55);
+    f1.show();
+    return 0;
+}
+*/
