b8170c8c5befacd8cfb47216df1fd8b3f383b273
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-08-24 14:44:15 +0600

    2c korsi

--- OOP/sp23-mine/sp23ALL.cpp
+++ OOP/sp23-mine/sp23ALL.cpp
@@ -1,5 +1,3 @@
-
-
 // ! 2b
 /*
 #include<iostream>
@@ -20,7 +18,7 @@ public:
     }
 
 
-};
+};//
 int main()
 {
     flower f1(12,10), f2(1,55);
@@ -28,3 +26,48 @@ int main()
     return 0;
 }
 */
+
+// ! 2C
+
+#include<bits/stdc++.h>
+using namespace std;
+class Time{
+    
+   int hour , minutes;
+   public:
+  /*
+   Time(int x, int y)// ! eibabe o likte partam consturctor ta
+   {
+       hour = x;
+       minutes = y;
+   }*/
+   
+        Time(int hour , int minutes)
+        {
+            this -> hour = hour;
+            this -> minutes = minutes;
+        }
+   
+    friend void addTime(Time ,Time );// ? private data o access korte partese //tarpor object name na likleo run kortese
+};
+ 
+ void addTime(Time t1, Time t2)
+   {
+    int Hr = t1.hour +t2.hour + (t1.minutes + t2.minutes)/60;
+    int mn = (t1.minutes + t2.minutes) % 60;
+    cout << Hr << " Hour " << mn << " Minutes " << endl; 
+      
+   }
+int main()
+{
+   Time T1(12, 10), T2(1, 55);
+   addTime(T1, T2);
+    //output 14 Hour 5 Minutes 
+
+  
+   
+   return 0;
+}
+
+
+
